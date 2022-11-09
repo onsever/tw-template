e431413710bdab1e1b626acee2442a19026af844
Onurcan Sever <onsever@icloud.com>
2022-11-09 11:41:39 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      container: {
+        screens: {
+          xs: "375px",
+          sm: "640px",
+          md: "768px",
+          lg: "1024px",
+          xl: "1140px",
+          "2xl": "1140px",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
